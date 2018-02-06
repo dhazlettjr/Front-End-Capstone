36e421aa0800ac35c84176663b027d91ad9ba527
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-05 18:36:22 -0600

    created auth factory

---
+++ app/factories/StorageFactory.js
@@ -0,0 +1,17 @@
+
+// "use strict";
+
+// angular.module("rent").factory("StorageFactory", function ($q, $http, FBCreds) {
+
+//   let postToFB = () => {
+//     return $q(function (resolve, reject) {
+//       $http.post(`${FBCreds.storageBucket}`)
+//         .then((data) => {
+//           resolve(data);
+//         })
+//         .catch((err) => {
+//           reject(err);
+//         });
+//     });
+// };
+// });
