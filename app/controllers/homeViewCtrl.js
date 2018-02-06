8a536a23b79b691b8c6d216200b4a2229aa2ad76
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-06 12:48:38 -0600

    re-worked home view controller to print vehicles

--- app/controllers/homeViewCtrl.js
+++ app/controllers/homeViewCtrl.js
@@ -1,6 +1,17 @@
 "use strict";
 
 angular.module("rent")
-.controller("homeViewCtrl", function($scope) {
-  $scope.message = "one of one";
-});
+  .controller("homeViewCtrl", function ($scope, ProductsFactory) {
+
+
+
+
+    ProductsFactory.getRides()
+      .then((rideData) => {
+        console.log("hey", rideData);
+
+        //object.entries is a simple way to push an object into an array
+        $scope.rideArr = rideData;
+      });
+
+  });
