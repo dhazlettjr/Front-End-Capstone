77a50ad99988f4896ef95a2965a05e66be282899
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-05 18:34:31 -0600

    created firebase storage to allow file upload

--- app/app.js
+++ app/app.js
@@ -1,5 +1,15 @@
 "use strict";
 
+let config = {
+    apiKey: "AIzaSyDQHt0Mzr35CueolsDnVieirIfocLGWXZU",
+    authDomain: "rent-46120.firebaseapp.com",
+    databaseURL: "https://rent-46120.firebaseio.com",
+    projectId: "rent-46120",
+    storageBucket: "rent-46120.appspot.com",
+    messagingSenderId: "161015486110"
+  };
+  firebase.initializeApp(config);
+
 angular.module("rent", ["ngRoute"])
 
 .config($routeProvider => {
@@ -8,43 +18,19 @@ angular.module("rent", ["ngRoute"])
     // routes and configs go here, chained onto the module definition
     
     
-    .when("/login", {
-        templateUrl: "",
-        controller: ""
+    .when("/home", {
+        templateUrl: "partials/homeView.html",
+        controller: "UploadCtrl"
     })
-    .when ("/", {
-        templateUrl: "",
-        controller: ""
+    .when ("/login", {
+        templateUrl: "partials/Login.html",
+        controller: "AuthCtrl"
     })
-    .when("/board/new", {
-        templateUrl: "",
-        controller: ""
-    })
-    .when ("", {
-        templateUrl: "",
-        controller: ""
-    })
-    .when("/board/:bid/edit", {
-        templateUrl: "",
-        controller: ""
-    })
-    .when ("", {
-        templateUrl: "",
-        controller: ""
-    })
-    .when ("", {
-        templateUrl: "",
-        controller: ""
-    })
-    .when ("", {
-        templateUrl: "",
-        controller: ""
-    })
-    .otherwise("/login");
+    .otherwise("/");
     
 });
 
-// .run(FBCreds => {
-//   firebase.initializeApp(FBCreds);
-// });
+//   .run(FBCreds => {
+//     firebase.initializeApp(FBCreds);
+//  });
 
