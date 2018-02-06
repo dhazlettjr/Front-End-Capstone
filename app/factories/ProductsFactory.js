6c10fa5dd697f30ffa8466783b7db78e57aafa0c
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-06 12:50:31 -0600

    created a factory to get and post

---
+++ app/factories/ProductsFactory.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// initialize the module to encorporate the factory
+angular.module("rent").factory("ProductsFactory", function ($q, $http, FBCreds) {
+
+    // Return a promise with XHR
+    let getRides = () => {
+        return $q(function (resolve, reject) {
+            $http
+                .get(`${FBCreds.url}/vehicles.json`)
+                .then(
+                    ({
+                        data
+                    }) => {
+                        //let keys = Object.keys(data);
+                        //console.log(uid);
+                        //keys.forEach(key => data[key].uid = key);
+                        resolve(data);
+                    })
+                .catch((err) => {
+                    reject(err);
+                });
+        });
+    };
+
+    let saveRides = (newList) => {
+        return $q(function (resolve, reject) {
+            $http
+                .post(`${FBCreds.url}/vehicles.json`, JSON.stringify(newList))
+                .then(
+                    (data) => {
+                        resolve(data);
+                        console.log("new items posted", data);
+                    })
+                .catch((err) => {
+                    reject(err);
+                });
+        });
+    };
+    // this will return the firebase data
+    return {
+        getRides,
+        saveRides
+    };
+});
