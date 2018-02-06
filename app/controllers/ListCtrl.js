32a0a3b2fb2382eee0c6846771d02e82a4067d70
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-06 12:50:11 -0600

    created a controller to list items to dom

---
+++ app/controllers/ListCtrl.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+// this will set up the controller for the project
+angular.module("rent").controller("ListCtrl", function($scope, $location, ProductsFactory) {
+
+    $scope.newList = {
+        type: "",
+        make: "",
+        model: "",
+        location: "",
+        year: "",
+        price: "",
+        description: "",
+        features: "",
+        img: "",
+        uid: ""
+    };
+
+    $scope.saveRides = () => {
+        console.log('new ride to save', $scope.newList);
+        ProductsFactory.saveRides($scope.newList);
+        $location.url("/list");
+
+    };
+
+});
