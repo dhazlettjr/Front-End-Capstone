52c79ab8892e009d2aaa7f80f349cba29de524a7
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-07 11:54:54 -0600

    created function for printing uid rides and deleting rides

--- app/controllers/MyListCtrl.js
+++ app/controllers/MyListCtrl.js
@@ -1,16 +1,28 @@
 // "use strict";
 
-// angular.module("rent")
-//   .controller("MyListCtrl", function ($scope, ProductsFactory) {
+"use strict";
+
+angular.module("rent")
+  .controller("MyListCtrl", function ($scope,$location, $route, ProductsFactory) {
 
 
+$scope.deleteRides = (carID) => {
+    ProductsFactory.deleteRides(carID)
+    .then(() => {
+        ProductsFactory.getRides();
+        $location.url("#!/home");
+    });
+};
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        ProductsFactory.getRides(firebase.auth().currentUser.uid)
+          .then((data) => {
+            console.log("hey", data);
 
-
-//     ProductsFactory.getRides()
-//       .then((rideData) => {
-//         console.log("hey", rideData);
-
-//         //object.entries is a simple way to push an object into an array
-//         $scope.rideArr = rideData.id;
-//       });
-//     });
+            //object.entries is a simple way to push an object into an array
+            $scope.rideArr = Object.values(data);
+            console.log($scope.rideArr);
+          });
+      }
+    });
+  });
