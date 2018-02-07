71b6b1d57fb3d0b7f6936c88b8ba9a056a8e1847
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-06 19:39:15 -0600

    created new ELs, added storageRef to send storage image to database

--- app/controllers/ListCtrl.js
+++ app/controllers/ListCtrl.js
@@ -1,8 +1,7 @@
-
 'use strict';
 
 // this will set up the controller for the project
-angular.module("rent").controller("ListCtrl", function($scope, $location, ProductsFactory) {
+angular.module("rent").controller("ListCtrl", function ($scope, $location, StorageFactory, ProductsFactory) {
 
     $scope.newList = {
         type: "",
@@ -12,16 +11,38 @@ angular.module("rent").controller("ListCtrl", function($scope, $location, Produc
         year: "",
         price: "",
         description: "",
-        features: "",
-        img: storageRef.getDownloadURL(),
-        uid: ""
+        features: ""
     };
+
+    let fileButton = document.getElementById('fileButton');
+    let uploader = document.getElementById('uploader');
+    let file;
+    let storageRef;
+
+
+    fileButton.addEventListener('change', function (e) {
+        file = e.target.files[0];
+        storageRef = firebase.storage().ref(file.name);
+        StorageFactory.pushFile(e, uploader);
+    });
 
     $scope.saveRides = () => {
-        console.log('new ride to save', $scope.newList);
-        ProductsFactory.saveRides($scope.newList);
-        $location.url("/list");
+
+        storageRef.getDownloadURL().then(function (url) {
+            console.log(url);
+            $scope.newList.img = url;
+            ProductsFactory.saveRides($scope.newList);
+            $location.url("/list");
+            $scope.ride.id = firebase.auth().currentUser.uid;
+            ProductsFactory.addRide($scope.ride)
+                .then((data) => {
+
+                });
+            console.log('new ride to save', $scope.newList);
+        });
 
     };
-
 });
+
+//let file = e.target.files[0];
+//let storageRef = firebase.storage().ref(file.name);
