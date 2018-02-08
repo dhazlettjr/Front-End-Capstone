cc8c099e4ec68b04f55df6b4d4f303177e144fdd
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-08 11:27:40 -0600

    added comments

--- app/controllers/ListCtrl.js
+++ app/controllers/ListCtrl.js
@@ -3,6 +3,7 @@
 // this will set up the controller for the project
 angular.module("rent").controller("ListCtrl", function ($scope, $location, StorageFactory, ProductsFactory) {
 
+    //this is attached to the input field partial
     $scope.newList = {
         type: "",
         make: "",
@@ -19,7 +20,7 @@ angular.module("rent").controller("ListCtrl", function ($scope, $location, Stora
     let file;
     let storageRef;
 
-
+//EL to upload file to FB storage
     fileButton.addEventListener('change', function (e) {
         file = e.target.files[0];
         storageRef = firebase.storage().ref(file.name);
@@ -28,14 +29,17 @@ angular.module("rent").controller("ListCtrl", function ($scope, $location, Stora
 
     $scope.saveRides = () => {
 
+        //function to add FB storage im to FB database
         storageRef.getDownloadURL().then(function (url) {
             console.log(url);
             $scope.newList.img = url;
-            // ProductsFactory.saveRides($scope.newList);
             $location.url("/list");
+            // this only deletes the car with that ID
             $scope.newList.uid = firebase.auth().currentUser.uid;
             ProductsFactory.saveRides($scope.newList)
                 .then((data) => {
+            // once data is saved, it will return you to the home page
+                    $location.url("/home");
 
                 });
             console.log('new ride to save', $scope.newList);
@@ -43,6 +47,3 @@ angular.module("rent").controller("ListCtrl", function ($scope, $location, Stora
 
     };
 });
-
-//let file = e.target.files[0];
-//let storageRef = firebase.storage().ref(file.name);
