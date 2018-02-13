05f730e7f5fd2c9930a1ee53fa439ead1ebbde82
dhazlettjr <d.hazlett.jr@gmail.com>
2018-02-13 10:48:47 -0600

    new controller to grab data for map pins

--- app/controllers/mapCtrl.js
+++ app/controllers/mapCtrl.js
@@ -1,15 +1,30 @@
 "use strict";
 
 angular.module("rent")
-  .controller("mapCtrl", function ($scope,ngMap, ProductsFactory) {
+  .controller("mapCtrl", function ($scope,NgMap, ProductsFactory) {
+
+    let getAllRides = () => {
+
+        ProductsFactory.getAllRides()
+          .then((data) => {
+  
+            //object.entries is a simple way to push an object into an array
+            $scope.rideArr = Object.entries(data.data);
+            console.log('ride',$scope.rideArr);
+          });
+
+      };
+      getAllRides();
 
     NgMap.getMap().then(function (map) {
         $scope.showCustomMarker = function (evt) {
             map.customMarkers.foo.setVisible(true);
             map.customMarkers.foo.setPosition(this.getPosition());
+            
         };
         $scope.closeCustomMarker = function (evt) {
             this.style.display = 'none';
         };
+
     });
 });
